b5c8e643567e4bf68ea7d555c2e9aabcec1e0e75
Jaedson Silva <contacanal798@gmail.com>
2022-05-08 03:19:16 -0300

    creating Root class to manage music player window

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+class Root:
+    def __init__(self, window: Tk):
+        self.window = window
+
+        self.window.geometry('400x400')
+        self.window.title('PyPlayer')
+        self.window.resizable(0, 0)
+
+        self.window.mainloop()
