cfc39448768ec567dd9422c944dc88d4f1295e26
Jaedson Silva <contacanal798@gmail.com>
2022-05-08 03:16:45 -0300

    adding find_musics method in Player class

--- player.py
+++ player.py
@@ -7,3 +7,12 @@ class Player:
     def __init__(self) -> None:
         self.mixer = mixer
         self.mixer.init()
+
+    def find_musics(self, dirpath: str) -> list:
+        def filter_music(filename: str) -> Union[str, None]:
+            if filename.endswith('.mp3') or filename.endswith('.mp4a'):
+                return filename
+
+        if os.path.isdir(dirpath):
+            musics: list = os.listdir(dirpath)
+            filter(filter_music, musics)
